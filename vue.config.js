95a21e9d6b69061f88c704357c45795fbf15e7cc
huahua0406 <623072902@qq.com>
2019-08-22 02:01:16 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 // vue.config.js
+const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
     publicPath: '/',
     lintOnSave: false,
+    productionSourceMap: false,
     devServer: {
         open: true, // 配置自动启动浏览器
         proxy: {
@@ -12,6 +18,17 @@ module.exports = {
                     '^/api': ''
                 }
             }
+        }
+    },
+    configureWebpack: config => {
+        if (isProduction) {
+            config.plugins.push(new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 10240,
+                minRatio: 0.8
+            })
+            )
         }
     }
 };
